195a08e982aa9143e46de698559b107832d7e156
Abhishek Kumar <sharmaabhishek098@gmail.com>
2020-12-26 00:18:00 -0800

    Add files via upload

--- src/pages/pool/Pool.jsx
+++ src/pages/pool/Pool.jsx
@@ -1,11 +1,55 @@
 import React, { Component } from 'react'
+import advimg from '../../images/adv-1.png'
+import liquiditybgimg from '../../images/liquidity-rewards-img.png'
+import liquiditybggradient from '../../images/liquidity-rewards-noise.png'
+import advimg2 from '../../images/adv-2.png'
+import './Pool.scss'
 
-export default class Pool extends Component {
+class Pool extends Component {
     render() {
         return (
-            <div>
-                POOL VBBBBBB
+            <div className="pool-container">
+                <div className="adv-img-container">
+                    <img src={advimg} />
+                </div>
+                <div className="liquidity-container">
+                    <div className="liquidity-wrap">
+                        <div className="liquidity-rewards-bg-gradient">
+                            <div className="liquidity-rewards-bg-img" style={{ backgroundImage: `url(${liquiditybgimg})` }}></div>
+                            <div className="liquidity-rewards-overlay" style={{ backgroundImage: `url(${liquiditybggradient})` }}></div>
+                            <div className="liquidity-content">
+                                <h3>Liquidity provider rewards</h3>
+                                <p>Liquidity providers earn a 0.2% fee on all trades proportional to their share of the pool. Fees are added to the pool, accrue in real time and can be claimed by withdrawing your liquidity.</p>
+                            </div>
+                            <div className="liquidity-rewards-bg-img" style={{ backgroundImage: `url(${liquiditybgimg})` }}></div>
+                            <div className="liquidity-rewards-overlay" style={{ backgroundImage: `url(${liquiditybggradient})` }}></div>
+                        </div>
+                        <div className="liquidity-picker">
+                            <div className="liquidity-row-1">
+                                <label>Your liquidity</label>
+                                <div className="btn-wrap">
+                                    <button className="btn btn-stroke mr-2">Create a pair</button>
+                                    <button className="btn btn-yellow">Add Liquidity</button>
+                                </div>
+                            </div>
+                            <div className="liquidity-row-2">
+                            Connect to a wallet to view your liquidity.
+                            </div>
+                            <div className="liquidity-row-3">
+                            Don't see a pool you joined?
+                            <a id="import-pool-link" href="javascript:void(0)">Import it.</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="adv-img-container adv-img-2">
+                    <img src={advimg2} />
+                </div>
+                <a href="javascript:void(0)" target="_blank" className="btn btn-back-to-home">
+                    <span>&nbsp;</span>
+                </a>
             </div>
         )
     }
 }
+export default Pool
